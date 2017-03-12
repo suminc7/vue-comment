dd1f5afef16a2fb5eebd650bd013f9be8cc06014
sumin <suminc7@gmail.com>
2017-03-12 19:16:48 +0900

    add book model

--- index.js
+++ index.js
@@ -18,3 +18,34 @@ app.listen(app.get('port'), function() {
 });
 
 
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+    console.log('connected!')
+    var Book = require('./models/book');
+
+
+    var book = new Book();
+    book.title = 'hi';
+    book.author = 'sumin';
+    book.published_date = new Date();
+
+    book.save(function(err){
+        if(err){
+            console.error(err);
+            // res.json({result: 0});
+            return;
+        }
+
+        // res.json({result: 1});
+
+    });
+});
+
+
